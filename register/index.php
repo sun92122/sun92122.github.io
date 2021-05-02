ba7acc74dad62b9c8a0b25d984b8fec69b9737a0
sun92122 <sun920122@gmail.com>
2021-05-02 17:17:20 +0800

    clone from keedos

---
+++ register/index.php
@@ -0,0 +1,133 @@
+<?php
+ob_start();
+define('TITLE', "Signup");
+include '../assets/layouts/header.php';
+check_logged_out();
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="description" content="Keedos 是一個為學生打造的人才媒合平台。你可以在這裡存下你參與各種專案的機會，亦或者是透過我們的搜索引擎尋找到最適合和你一起創造奇蹟的夥伴。一起在高中生涯之中創造屬於你我的奇蹟。">
+    <title>keedos official</title>
+    <!--<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">-->
+</head>
+
+<body>
+    <div class="login-clean" style="background: rgb(255,255,255);"><a class="btn btn-primary" role="button" style="background: url(&quot;../assets/img/Back.svg&quot;) left no-repeat;margin-left: 50px;border-style: none;border-top-style: none;border-right-color: rgb(255,255,255);border-left-color: rgb(255,255,255);" href="../index"></a>
+        <form class="form-auth" action="includes/register.inc.php" method="post" enctype="multipart/form-data" style="border-color: rgb(255,255,255);">
+
+            <?php insert_csrf_token(); ?>
+
+
+
+            <div class="illustration"><strong style="padding: 15px;text-align: center;font-family:FakePearl-Regular;">註冊</strong></div>
+
+            <div class="text-center mb-3">
+                <small class="text-success font-weight-bold">
+                    <?php
+                        if (isset($_SESSION['STATUS']['signupstatus']))
+                            echo $_SESSION['STATUS']['signupstatus'];
+
+                    ?>
+                </small>
+            </div>
+
+            <div class="form-group">
+              <input class="form-control" type="text" id="username" name="username" placeholder="帳號" required autofocus="" style="border-radius: 16px;text-align: left;">
+              <sub class="text-danger">
+                  <?php
+                      if (isset($_SESSION['ERRORS']['usernameerror']))
+                          echo $_SESSION['ERRORS']['usernameerror'];
+
+                  ?>
+              </sub>
+            </div>
+
+            <div class="form-group">
+              <input class="form-control" type="email" id="email" name="email" placeholder="Email" inputmode="email" required style="border-radius: 16px;">
+              <sub class="text-danger">
+                  <?php
+                      if (isset($_SESSION['ERRORS']['emailerror']))
+                          echo $_SESSION['ERRORS']['emailerror'];
+
+                  ?>
+              </sub>
+            </div>
+
+            <div class="form-group">
+              <input class="form-control" type="password" id="password" name="password" placeholder="密碼" inputmode="verbatim" required style="border-radius: 16px;">
+            </div>
+
+            <div class="form-group">
+              <input class="form-control" type="password" id="confirmpassword" name="confirmpassword" placeholder="確認密碼" inputmode="verbatim" required style="border-radius: 16px;">
+              <sub class="text-danger mb-4">
+                  <?php
+                      if (isset($_SESSION['ERRORS']['passworderror']))
+                          echo $_SESSION['ERRORS']['passworderror'];
+
+                  ?>
+              </sub>
+            </div>
+            <div class="form-group">
+                <div class="form-row" style="text-align: center;margin: 0px;opacity: 1;">
+                    <div class="col">
+                        <p></p>
+                    </div>
+                    <div class="col" style="padding: 0px;padding-right: 0px;">
+                    <button class="btn btn-primary btn-block" type="submit" value="signupsubmit" name='signupsubmit' style="text-align: center;border-radius: 40px;font-family: Poppins;background: #f08b33;font-family: Poppins;font-style: normal;font-weight: 600;font-size: 14px;line-height: 24px;align-items: center;text-align: center;letter-spacing: 0.75px;color: #ffffff;width: 184px;height: 40px;">
+                        繼續</button>
+
+                    </div>
+                    <div class="col">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div class="form-row">
+                <div class="col"><a class="forgot" href="../login">註冊過了?</a></div>
+                <div class="col"><a class="forgot" href="../reset-password">忘記密碼?</a></div>
+            </div>
+        </form>
+    </div>
+    <!--<script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>-->
+</body>
+
+</html>
+
+
+
+<script type="text/javascript">
+    function readURL(input) {
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#avatar").change(function() {
+        console.log("here");
+        readURL(this);
+    });
+</script>
---
+++ resume/includes/edit-resume.inc.php
@@ -0,0 +1,83 @@
+<?php
+
+session_start();
+
+require '../../assets/includes/auth_functions.php';
+require '../../assets/includes/datacheck.php';
+require '../../assets/includes/security_functions.php';
+
+
+//if (!isset($_POST['resumesubmit'])){
+
+  //  header("Location: ../");
+  //  exit();
+//}
+//else {
+
+    /*
+    * -------------------------------------------------------------------------------
+    *   Securing against Header Injection
+    * -------------------------------------------------------------------------------
+    *
+
+    foreach($_POST as $key => $value){
+
+        $_POST[$key] = _cleaninjections(trim($value));
+    }*/
+
+
+    /*
+    * -------------------------------------------------------------------------------
+    *   Verifying CSRF token
+    * -------------------------------------------------------------------------------
+    *
+
+    if (!verify_csrf_token()){
+
+        $_SESSION['STATUS']['loginstatus'] = 'Request could not be validated';
+        header("Location: ../");
+        exit();
+    }*/
+
+
+    //require '../../assets/setup/db.inc.php';
+    require("../../assets/vendor/PHPDatabase/easyCRUD/Person.class.php");
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $hashtag1 = $_POST['hashtag1'];
+    $hashtag2 = $_POST['hashtag2'];
+    $hashtag3 = $_POST['hashtag3'];
+    $keyword = $hashtag1.$hashtag2.$hashtag3;
+    $job_title = $_POST['job_title'];
+    $work_way = $_POST['work_way'];
+    $email = $_POST['email'];
+    $social_media = $_POST['social_media'];
+    $phone = $_POST['phone'];
+    $portfolio = $_POST['portfolio'];
+    $self_intro = $_POST['self_intro'];
+
+
+    // Instantiate the person class
+    $person  = new Person();
+
+    // Create new person
+    $person->Name = $name;
+    $person->Gender = $gender;
+    $person->Keyword  = $keyword;
+    $person->Age = $age;
+    $person->Job_title = $job_title;
+    $person->Work_way = $work_way;
+    $person->Email = $email;
+    $person->Social_Media = $social_media;
+    $person->Phone = $phone;
+    $person->Portfolio = $portfolio;
+    $person->Introduction = $self_intro;
+    //$person->Experience = $experience;
+    $creation = $person->Create();
+    header("Location: ../");
+
+
+
+
+//}
